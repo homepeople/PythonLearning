9b3af2b9e7d89a23be62bb77f221a95031c8c793
Fred Zeng <homepeopleg@gmail.com>
2018-07-10 17:06:04 +0800

    2 def about Print Factor and Prime，one is for largest factor,the other is for minimum

--- src/像计算机科学家一样思考Python_Exercise3/Lesson013/WhileLess.py
+++ src/像计算机科学家一样思考Python_Exercise3/Lesson013/WhileLess.py
@@ -1,4 +1,43 @@
-import string
-print(string.punctuation)
-print(string.whitespace)
-# def countwords():
+#coding=UTF-8
+#largest factor or prime
+#最大公约数或是素数(质数)
+
+def showMaxFactor(num): 
+    for eachNum in range(2, num): 
+        count = eachNum // 2 
+        while count > 1: 
+            if (eachNum % count == 0): 
+                print ('largest factor of',eachNum,'is', count)
+                break 
+            count = count - 1  
+        else: 
+            print (eachNum, 'is prime' )
+        
+def FastPrintPrime(num):
+    i = 2
+    while(i < num):
+        j = 2
+        while(j <= (i/j)):
+            if not(i%j): 
+                print ('minimum factor of',i,'is', j)
+                break
+            j = j + 1
+        else: 
+            print (i, 'is prime' ), "是素数(质数)its prime"
+        i = i + 1
+        
+if __name__ == '__main__':
+    from time import clock
+    start=clock()
+#     FastPrintPrime(10000)
+    showMaxFactor(10000) 
+    finish=clock()
+    print (finish-start)
+
+   
+#     start=clock()
+# #     for eachNum in range(2, 501): 
+#    
+#         
+#     finish=clock()
+#     print (finish-start)
