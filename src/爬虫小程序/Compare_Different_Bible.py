717a956676751a592215df79fdca7d49dd99ac96
Administrator <Administrator@192.168.6.164>
2020-03-26 04:20:40 +0800

    add some commit

--- src/爬虫小程序/Compare_Different_Bible.py
+++ src/爬虫小程序/Compare_Different_Bible.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
+'''
+compare different version of Bible by each verse,and put the different verse together, 
+if the verse content are same then the verse print once and plus their version info together
+比较不同版本的圣经的每一节经文，并将不同版本的同一节经文放在一起，相同经文只打印一次并把版本信息合并
+'''
 import urllib.request
 from bs4 import BeautifulSoup
 import re
 import time
-from tkinter import *
+#  from tkinter import *
+from Tk import Tkinter
+
 
 '''
 Regular Expression function to solve string
 '''
 def getLeftNumber(tmpStr):#input string,return all number of string at left side.输入字符串，返回靠左连续数字的位数
-    for digitNum in range(len(tmpStr),0,-1):
-        tmpStr=tmpStr[0:digitNum]
+    for digitPos in range(len(tmpStr),0,-1):
+        tmpStr=tmpStr[0:digitPos]
         if tmpStr.isdigit():
-            return digitNum
-        
+            return digitPos
+
 def cut_out(StrA,StrB,Tstr):#find the value between StrA and StrB from Tstr.在Tstr中截取StrA到StrB之间的值
     result = re.findall(".*%s(.*)%s.*" %(StrA,StrB),Tstr)
     for i in result:
        return i
-  
-  
-    '''
-    Get Information from Original URL
-    '''
 
+'''
+    Get Information from Original URL
+'''
 def getVersion(url):#get language + version of Bible from original url,return a string
     version='('+ cut_out('lp-','/\d+/\d+',url) + ')'   
     return version  
@@ -40,7 +45,7 @@ def getChapterNumber(url):#get Chapter of Bible from original url,return a strin
 Get Text from Html of URL
 '''
 def get_TupleofChapter_FromUrl(url,CodeStyle,TagetTitle,TagetID):#get Text from html by BeautifulSoup,return tuple
-    htmls=getHtmlWithDecode(url, CodeStyle)
+    htmls=getHtmlWithDecode(url, CodeStyle)#with or without decode,which one is better?
     bf = BeautifulSoup(htmls,"html.parser")
     texts = bf.find_all(TagetTitle, id=TagetID) 
     TupleofChapter=arrange_Line_FirstNum(texts[0].text,url)#divide text by firstNumber to get each verse in chapter
@@ -80,23 +85,23 @@ def arrange_Line_FirstNum(tmpStr,url): #divide line to a list by the beginning n
             if  re.match(pat,tmpStr[positionN-1]) != None or chaNum==0 :    
                 chaNum=getChapterNumber(url)                            #Get chapter string from url
                # chaNum=cut_out('/','#study=discover',url)              #Get chapter string from url
-                digitNum = getLeftNumber(tmpStr[positionN:positionN+3])  #没有3位数以上的章节,因此只判断3位数 
+                digitPos = getLeftNumber(tmpStr[positionN:positionN+3])  #没有3位数以上的章节,因此只判断3位数 
                   
-                if digitNum != 4:                                      #没有3位数及以上的章节   
+                if digitPos != 4:                                      #digit position of chapter is not longer than 3,没有3位数及以上的章节   
                      
-                    aheadstr = tmpStr[positionN:positionN+digitNum]    #句头数字
+                    aheadstr = tmpStr[positionN:positionN+digitPos]    #句头数字
                       
-                    if aheadstr ==str(secNum+1) or (aheadstr == chaNum and secNum == 0):      #句头数字等于节数或章数
+                    if aheadstr ==str(secNum+1) or (aheadstr == chaNum and secNum == 0): #first Number is number of verse or chapter句头数字等于节数或章数
                           
                         Tmp_Tuple += (tmpStr[positionL:positionN],)
                         positionL = positionN  
                         secNum += 1                        
         positionN += 1
    
-    Tmp_Tuple += (tmpStr[positionL:positionN],)#加入最后一行数据
+    Tmp_Tuple += (tmpStr[positionL:positionN],)#add the last line 加入最后一行数据
     return Tmp_Tuple
-               
-def combine_Tuples(versionT,*tupleX): #zip tuples as a list of dictionary unit
+          
+def combine_Tuples(versionT,*tupleX): #zip tuples as a list with dictionary item,return the list
     printList=[] 
     newList=[] 
     for tup in zip(*tupleX):#zip tuples to list with dictionary item
@@ -107,7 +112,7 @@ def combine_Tuples(versionT,*tupleX): #zip tuples as a list of dictionary unit
             printList.append(tmpDicA)
             n+=1 
             
-    for x in printList[::len(versionT)]:#merge same content of different version from many dictionary
+    for x in printList[::len(versionT)]:#merge same content of different version(content are keys,version are value of dictionary) from many dictionary
         newList.append(merg_dict(printList[:len(versionT)]))
         printList=printList[len(versionT):]
         newList.append('')
@@ -121,15 +126,15 @@ def printListWithDictUnit(newList):#Print every List Unit which is Dictionary
                 print('{key} {value}'.format(key = key, value = value))
         else:
             print(dict)
-                  
-def merg_dict(list):#merg many dictionary of a list
+
+def merg_dict(list):#merg many dictionary of a list,return a dictionary
     dict2={}
     for dict1 in list:
         dict2=dict_union(dict1, dict2)
         
     return dict2   
 
-def dict_union(dica, dicb):#unite 2 dictionary to new one,return new one
+def dict_union(dica, dicb):#unite 2 dictionary to new one,return the new dictionary
     dic={}
     for key in dica:
         if dicb.get(key):#if key are same,plus their vaules
@@ -143,57 +148,63 @@ def dict_union(dica, dicb):#unite 2 dictionary to new one,return new one
             dic[key]=dicb[key]
 
     return dic
- 
+
 '''
- UI interface backward
-'''    
-def MainUI():
+ UI interface backward #draft
+'''
+def MainUI():#draft
     top=Tk.Tk()
     B = Tk.Button(top, text ="Run", command = CallAllBack)
     B.pack()
     top.mainloop() 
-    
-class Application(Frame):
+
+class Application(Frame):#draft
     def ___init___(self,master=None):
         Frame.__init__(self,master)
         self.grid()
-#          self.createWidgets()
-#      def createWidgets(self):
-#          self.quitButton= Button(self, text ="Run", command = self.quit())
-#          self.quitButton.grid()
-        
+        self.createWidgets()
+    def createWidgets(self):
+        self.quitButton= Button(self, text ="Run", command = self.quit)
+        self.quitButton.grid()
+
 def CallAllBack():
     CodeStyle='UTF-8'
     TargetTitle='div'
     TargetID="article"
     book='43'
     chapter='7'
-     #input url where the book and the chapter should be the same  
+     #input url where the book and the chapter should be the same
     url1='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/nwt/'+book+'/'+chapter+'#study=discover'
     url2='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/bi12/'+book+'/'+chapter+'#study=discover'
     url3='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/sbi1/'+book+'/'+chapter+'#study=discover'
     url4='https://wol.jw.org/en/wol/b/r1/lp-e/nwt/'+book+'/'+chapter+'#study=discover'
-    
+
     version1=getVersion(url1)
     version2=getVersion(url2)
     version3=getVersion(url3)
     version4=getVersion(url4)
     versionT=(version1,version2,version3,version4)
-      
+
     Tuple1=get_TupleofChapter_FromUrl(url1,CodeStyle,TargetTitle,TargetID)
     Tuple2=get_TupleofChapter_FromUrl(url2,CodeStyle,TargetTitle,TargetID)
     Tuple3=get_TupleofChapter_FromUrl(url3,CodeStyle,TargetTitle,TargetID)
     Tuple4=get_TupleofChapter_FromUrl(url4,CodeStyle,TargetTitle,TargetID)
-      
+
     newList=combine_Tuples(versionT,Tuple1,Tuple2,Tuple3,Tuple4)
     printListWithDictUnit(newList)
 
+def callAllback1(bookNum,Chapter,*URL):#draft
+    CodeStyle='UTF-8'
+    TargetTitle='div'
+    TargetID="article"
+    for url in URL:
+        cut_out(StrA, StrB, Tstr)
 if __name__ == '__main__':
 #      app=Application()
 #      app.master.title("Sample Application")
 #      app.mainloop()
 
-    CallAllBack()
-    
-#      MainUI()
+#      CallAllBack()
+
+    MainUI()
 
