e63ad67f73301f058d45f31c38f52e59c9e2f7a2
Administrator <Administrator@192.168.6.164>
2020-10-14 22:52:34 +0800

    multy chapters compare

--- src/爬虫小程序/Compare_Different_Bible.py
+++ src/爬虫小程序/Compare_Different_Bible.py
@@ -4,13 +4,19 @@
 compare different version of Bible by each verse,and put the different verse together, 
 if the verse content are same then the verse print once and plus their version info together
 比较不同版本的圣经的每一节经文，并将不同版本的同一节经文放在一起，相同经文只打印一次并把版本信息合并
+1.如果不同版本的同节经文完全一致，则该经文只打印一次，但版本号会在该节经文后叠加
+2.如果不同版本的同一章经文节数不同，则没有经文的节数会显示为None(例如：马可福音16章，约翰福音8章)
 '''
+
 import urllib.request
 import requests
-from bs4 import BeautifulSoup
+#  from bs4 import BeautifulSoup
+import bs4
 import re
 import time
 import itertools
+import http
+
 '''
 Regular Expression function to solve string
 '''
@@ -59,18 +65,29 @@ def get_Chapter_text_Tuple(url):#get Text from html by BeautifulSoup,return tupl
     TargetTitle = 'div'
     TargetID = "article"
     
-    htmls = get_html(url)#with or without decode,which one is better?
-    
-    bf = BeautifulSoup(htmls,"html.parser")
+    htmls = connect_URL_test(get_html,url) 
 
+    bf =bs4.BeautifulSoup(htmls,"html.parser")
+    
     texts = bf.find_all(TargetTitle, id=TargetID) 
 
-    ChapterTextTuple = divide_string_by_beggin_Number_of_senctence_withException(texts[0].text,url)#divide text by firstNumber to get each verse in chapter
+    ChapterTextTuple = divide_string_by_begin_Number_of_senctence_withException(texts[0].text,url)#divide text by firstNumber to get each verse in chapter
 
     return ChapterTextTuple
 
-
-def get_html_with_decode(url,CodeStyle):#Decode Html from URL with codeType，return StringHtml
+def connect_URL_test(get_html_fun,url):
+    try:
+        print("Now loading : %s" % url)
+        html = get_html_fun(url)
+    except urllib.request.HTTPError as exp:
+        print(exp.code)
+    except urllib.request.URLError as exp:
+        print(exp.reason)
+    else:
+        print("loaded: %s" % url);
+        return html
+    
+def get_decode_html(url,CodeStyle):#Decode Html from URL with codeType，return StringHtml
 
     response = urllib.request.urlopen(url)
     
@@ -93,7 +110,7 @@ def get_html(url):#get Html Without decode,return html
 '''
 Arrange The Text and combine some different Bible as one as getting the some verse together
 '''
-def divide_string_by_beggin_Number_of_senctence_normal(tmpStr,url): #divide a long string to a Tuple by the number at begging of sentence,return new Tuple，根据每句开头数字分行建立新表,并返回分行后的新表
+def divide_string_by_begin_Number_of_senctence_general(tmpStr,url): #divide a long string to a Tuple by the number at begging of sentence,return new Tuple，根据每句开头数字分行建立新表,并返回分行后的新表
     positionN = 0  #N是Next，切片下一次的开始位置
     positionL = 0  #L是Last，切片上一次的结束位置
     Tmp_Tuple = ()
@@ -106,11 +123,9 @@ def divide_string_by_beggin_Number_of_senctence_normal(tmpStr,url): #divide a lo
           
         if char.isdigit() and positionN > 0 :                  #如果用isnumeric那么非阿拉伯数字也会算进去
               
-
             pat = u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])" 
-            nextValue = tmpStr[positionN-1]
-            if  re.match(pat,nextValue) != None or chaNum == 0 : #右边边不是数字，字符，中文字等一切符号或者是第一次调入
-                
+            if  re.match(pat,tmpStr[positionN-1]) != None or chaNum == 0 : #右边边不是数字，字符，中文字等一切符号或者是第一次调入
+                  
                 chaNum=get_chapter_number(url)                            #Get chapter number as string from url
   
                 numberLength = get_left_number(tmpStr[positionN:positionN+3])  
@@ -129,43 +144,43 @@ def divide_string_by_beggin_Number_of_senctence_normal(tmpStr,url): #divide a lo
     Tmp_Tuple += (tmpStr[positionL:positionN],)#add the last line 加入最后一行数据
     return Tmp_Tuple
   
-def divide_string_by_beggin_Number_of_senctence_withException(tmpStr,url): #divide a long string to a Tuple by the number at begging of sentence,return new Tuple，根据每句开头数字分行建立新表,并返回分行后的新表
-    positionN = 0  #N是Next，切片下一次的开始位置
-    positionL = 0  #L是Last，切片上一次的结束位置
+  
+def divide_string_by_begin_Number_of_senctence_withException(tmpStr,url): #divide a long string to a Tuple by the number at begging of sentence,return new Tuple，根据每句开头数字分行建立新表,并返回分行后的新表
+    positionN = 0  #N is Next,Mean Next slice position. N是Next，切片下一次的开始位置
+    positionL = 0  #L is Last,Mean Last slice position. L是Last，切片上一次的结束位置
     Tmp_Tuple = ()
     chaNum = 0
     verNum = 0
   
     tmpStr = tmpStr.replace('\n','').replace('+','').replace('*','').replace('. ','.').replace('。 ','。').replace('? ','?').replace('! ','!')
    
-    for char in tmpStr:#有数字开头的分行
+    for char in tmpStr:
           
-        if char.isdigit() and positionN > 0 :                  #如果用isnumeric那么非阿拉伯数字也会算进去
-            
+        if char.isdigit() and positionN > 0 :                  #if use isnumeric: no only 0-9 also count all kind of number like Chinese number.如果用isnumeric那么非阿拉伯数字也会算进去。
             pat = u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])" 
-         
-            nextValue = tmpStr[positionN-1]
-            if  re.match(pat,nextValue) != None or chaNum == 0 : #右边边不是数字，字符，中文字等一切符号或者是第一次调入
-                chaNum=get_chapter_number(url)  
+            
+            if  re.match(pat,tmpStr[positionN-1]) != None or chaNum == 0 :  #left position is not number,character,Chinese Word                   
+                chaNum = get_chapter_number(url)  
                 numberLength = get_left_number(tmpStr[positionN:positionN+3])  
-                  
+                
                 if numberLength < 4:                                      #length of chapter and verse number is less than 4,章节数小于4位   
                      
-                    aheadstr = tmpStr[positionN:positionN + numberLength]    
-                      
+                    aheadstr = tmpStr[positionN:positionN + numberLength]
+                     
                     if  int(aheadstr) - verNum == 1 or (aheadstr == chaNum and verNum == 0): #first Number is number of verse or chapter句头数字等于节数或章数
                         while len(Tmp_Tuple) < int(aheadstr)-1 and verNum > 1:#THE Exception for book 43 chapter 8 only,length of tuple should as same as verNum
                             Tmp_Tuple += (None,)
-                        Tmp_Tuple += (tmpStr[positionL:positionN],)
-                        positionL = positionN  
-                        verNum += 1 
-                              
-                    elif aheadstr == '12' and verNum == 1:#THE Exception for book 43 chapter 8 only,because the chapter start at verse 12
+                            
+                        if aheadstr != '30' or  len(tmpStr[positionL:]) > 6:#for Numbers chapter 20 verse 29
+                            Tmp_Tuple += (tmpStr[positionL:positionN],)
+                            positionL = positionN  
+                            verNum += 1 
+                                     
+                    elif aheadstr == '12' and verNum == 1 :#THE Exception for book 43 chapter 8 only,because the chapter start at verse 12
                         Tmp_Tuple += (tmpStr[positionL:positionN],)
                         positionL = positionN  
                         verNum = 12   
-                      
-                
+                               
         positionN += 1
    
     Tmp_Tuple += (tmpStr[positionL:positionN],)#add the last line 加入最后一行数据
@@ -199,6 +214,7 @@ def combine_Tuples(versionT,tupleChapter): #zip tuples as a list with dictionary
     else:
         return printList
     
+    
 def combine_Tuples_simple(versionT,tupleChapter): #exactly same as combine_Tuples can do,just read it easier
     printList = [] 
     newList = [] 
@@ -216,6 +232,7 @@ def combine_Tuples_simple(versionT,tupleChapter): #exactly same as combine_Tuple
         newList.append('')
         
     return newList
+
 
 def print_dict_unit_within_list(newList):#Print every List Unit which is Dictionary 
     for dict in newList:#print result 
@@ -267,9 +284,10 @@ def call_main(url):
 
     ChapterTextTuple = get_textT_from_URLT(get_Chapter_text_Tuple,url)#get chapter text as tuple
 
-    newList = combine_Tuples(versionTuple,ChapterTextTuple)# 
+    newList = combine_Tuples(versionTuple,ChapterTextTuple)
     
     print_dict_unit_within_list(newList)
+
 
 '''
  UI interface backward #draft
@@ -278,12 +296,16 @@ def call_main(url):
 
 if __name__ == '__main__':
 
-    BOOK='43'         #BOOK 1 is Genesis ，66 is Revelation,and so on
-    CHAPTER='8'
-    url1='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/nwt/'+BOOK+'/'+CHAPTER+'#study=discover'
-    url2='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/bi12/'+BOOK+'/'+CHAPTER+'#study=discover'
-    url3='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/sbi1/'+BOOK+'/'+CHAPTER+'#study=discover'
-    url4='https://wol.jw.org/en/wol/b/r1/lp-e/nwt/'+BOOK+'/'+CHAPTER+'#study=discover'
-  
-    call_main((url1,url2,url3,url4))
-   
+    BOOK='2'         #BOOK 1 is Genesis ，66 is Revelation,and so on
+    TempChap = 1
+    CHAPTER ='' 
+    Chapters=("33")
+    while TempChap <= len(Chapters):
+        CHAPTER = Chapters[TempChap - 1]
+        url1='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/nwt/'+BOOK+'/'+CHAPTER+'#study=discover'
+        url2='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/bi12/'+BOOK+'/'+CHAPTER+'#study=discover'
+        url3='https://wol.jw.org/cmn-Hans/wol/b/r23/lp-chs/sbi1/'+BOOK+'/'+CHAPTER+'#study=discover'
+        url4='https://wol.jw.org/en/wol/b/r1/lp-e/nwt/'+BOOK+'/'+CHAPTER+'#study=discover'
+        TempChap += 1
+        tupOfUrl=(url1,url2)
+        call_main(tupOfUrl)
